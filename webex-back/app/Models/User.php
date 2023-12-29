26b69d4d09ea2814b4819c9ea6ade7d59691db8f
NarineHay <naromisho87@gmail.com>
2023-12-28 22:02:05 -0800

    NH_roles

--- webex-back/app/Models/User.php
+++ webex-back/app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     protected $fillable = [
         'name',
