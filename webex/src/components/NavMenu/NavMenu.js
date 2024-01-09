f0ddbeb78fa763fdc2b7d9899510188d83ecfcd1
AshotWebDev <poghosyan.01@list.ru>
2024-01-09 09:29:14 +0400

    changer

--- webex/src/components/NavMenu/NavMenu.js
+++ webex/src/components/NavMenu/NavMenu.js
@@ -4,18 +4,30 @@ import { navIcon } from '../../iconFolder/icon'
 import './NavMenu.css'
 import { logoImage } from '../../images/images'
 import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 function NavMenu() {
     
-
-    
+     const { t, i18n } = useTranslation();
 
   return (
        <div className='nav-menu-div'>
             <NavLink to={'/'}>{logoImage}</NavLink>
              <ul className='nav-menu'>
-                <NavMenuItem index="1" path="/programing"/>
+                <div className='drop-item'>
+                    <li className='programing'>{t('navMenu.'+ '1')}</li>
+                    <ul className='drop-menu'>
+                         <NavMenuItem index="8" path="/1"/>
+                         <NavMenuItem index="9" path="/2"/>
+                         <NavMenuItem index="10" path="/3"/>     
+                         <NavMenuItem index="11" path="/3"/>     
+                         <NavMenuItem index="12" path="/3"/>     
+                         <NavMenuItem index="13" path="/3"/>     
+                    </ul> 
+
+                </div>
                 <NavMenuItem index="2" path="/projects"/>
+                <NavMenuItem index="7" path="/news"/>
                 <NavMenuItem index="3" path="/aboteus"/>
                 <NavMenuItem index="4" path="contact"/>
             </ul>
