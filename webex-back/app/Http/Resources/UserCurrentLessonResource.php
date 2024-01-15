839a27e5ca8b8dc2dcce8a506c611fea2e215f56
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-01-15 13:14:41 +0400

    change_api

--- webex-back/app/Http/Resources/UserCurrentLessonResource.php
+++ webex-back/app/Http/Resources/UserCurrentLessonResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CourseLanguage;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,13 +18,17 @@ class UserCurrentLessonResource extends JsonResource
 
         return [
           "id" => $this->id,
-          "user_id" => $this->user_id,
-          "course_language_id" => $this->course_languages->name,
-          "lesson_id"=>$this->lesson_id,
+          "course_language_id"=>$this->course_language_id,
+          "course_language_name" => $this->course_languages->name,
+          // "lesson_id"=>$this->lesson_id,
           //// "lessons" =>LessonResource::collection($this->lessons),
-          "lessons" =>$this->lessons->translation($request->lang),
+          // "current_lesson" =>$this->lessons->translation($request->lang),
+          "current_lesson" =>new CurrentLessonResource($this->lessons),
+          "lessons"=>$this->course_languages->lessons,
+          "lessons"=>LanguageLessonsResource::collection($this->course_languages->lessons),
+          "active_cource"=>$this->course_language_id,
+          "all_courses"=> CourseLanguage::all(),
 
-          "tasks"=>TasksResource::collection($this->lessons->tasks)
 
         ];
     }
