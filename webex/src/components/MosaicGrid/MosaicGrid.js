f6aaa75845034fa8a7999ab546db1573e851990f
AshotWebDev <poghosyan.01@list.ru>
2024-01-17 17:33:08 +0400

    navigatia

--- webex/src/components/MosaicGrid/MosaicGrid.js
+++ webex/src/components/MosaicGrid/MosaicGrid.js
@@ -5,10 +5,11 @@ import { Link} from 'react-router-dom';
 
 const MosaicGrid = () => {
 
+  const leng = localStorage.getItem('lang')
   return (
     <div className="mosaic-grid">
       {projectSectionData.map((el) => (
-        <Link key={el._id} to={`/projects/${el._id}`}>
+        <Link key={el._id} to={`/${leng}/projects/${el._id}`}>
           <div  className='mosaic-grid-item' >
           <img src={el.img} alt="" />
           <div className='mosaic-grid-item-text-div'>
