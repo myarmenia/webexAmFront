6097f2401ff8bf19be86585c9490305ee06e893a
NarineHay <naromisho87@gmail.com>
2024-01-18 14:29:12 +0400

    course lang changed

--- webex-back/app/Models/CourseLanguage.php
+++ webex-back/app/Models/CourseLanguage.php
@@ -10,9 +10,14 @@ class CourseLanguage extends Model
     use HasFactory;
     protected $guarded=[];
 
+    public function course_language_translations(){
+      return  $this->hasMany(CourseLanguageTranslation::class);
+    }
+
     public function lessons(){
       return  $this->hasMany(Lesson::class);
     }
+
     public function user_course_menegments(){
       return  $this->hasMany(UserCourseMenegment::class);
     }
---
+++ webex-back/public/assets/js/delete-file.js
@@ -0,0 +1,28 @@
+/**
+ * Account Settings - Account
+ */
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    (function () {
+
+        // Update/reset user image of account page
+        let file = document.getElementById('uploadedFile');
+        const fileInput = document.querySelector('.upload-file-input'),
+            resetFileInput = document.querySelector('.file-reset');
+
+        if (file) {
+            const resetFile = file.src;
+            fileInput.onchange = () => {
+                if (fileInput.files[0]) {
+                    file.src = window.URL.createObjectURL(fileInput.files[0]);
+                }
+            };
+            resetFileInput.onclick = () => {
+                fileInput.value = '';
+                file.src = resetFile;
+            };
+        }
+    })();
+});
