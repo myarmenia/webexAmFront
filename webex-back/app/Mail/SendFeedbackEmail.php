4dbb9078d7be4527b1d172102bb063915d6733f8
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-01-26 13:05:07 +0400

    add mail

---
+++ webex-back/app/Mail/SendFeedbackEmail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendFeedbackEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Обратной связь',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'layouts.mail.sendorder',
+        );
+    }
+
+    public function build()
+    {
+        return $this->with([
+                        'data' => $this->data,
+                    ])
+                    ->to('tradingtrader@mail.ru');
+                    // ->to(config('project.webex_email'));
+    }
+}
+
+
